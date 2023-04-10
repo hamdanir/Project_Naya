bbb9d1bffa2d7f5ce7cfaf8439c3a09f5ba41562
adlihardiyanto <testingerkamv2@gmail.com>
2023-04-10 10:34:06 +0700

    Update Homepage

--- js/homepage.js
+++ js/homepage.js
@@ -1,40 +1,28 @@
-let tbody = document.querySelector('tbody');
-
-let url = 'https://simple-rest-api-production-6482.up.railway.app/class';
-
-let myClasses = [];
-
-let data = {};
-
-function getClasses(){
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        Classes = data.data;
-        updateTable();
-        
-    })
-
-    
-}
-
-getClasses();
-
-function updateTable(){
-    let data = "";
-
-    if(Classes.length > 0){
-        for(i= 0;i < Classes.length;i++){
-
-            data +=  `<tr id="${Classes[i]['ID']}">
-                        <td>${Classes[i]['name']}</td>
-                        <td>${Classes[i]['level']}</td>
-                        <td>${Classes[i]['instructor']}</td>
-                        <td><button class="btn btn-danger" onclick="deleteMobile(event)">Join</button></td>
-                        <td><button class="btn btn-danger" onclick="deleteMobile(event)">Detail</button></td>   
-                     </tr>`
-        }
-     tbody.innerHTML = data;
+fetch('https://simple-rest-api-production-6482.up.railway.app/class')
+.then(function (response) {
+    if (response.status === 200) {
+        return response.json()
     }
+})
+.then(data => {
+    printBuku(data.data)
+})
 
+function printBuku(data) {
+    const booksContainer = document.querySelector('.books-cat .books');
+    let html = ``;
+    data.forEach((element) => {
+        html = html + `
+        <div class="book">
+            <div class="img"><img src="https://wpvip.edutopia.org/wp-content/uploads/2022/10/iStock-996990478.jpg?w=1300&quality=85"></div>
+            <div class="author">${element.name}</div>
+            <div class="title">${element.level}</div>
+            <div class="price">${element.instructor}</div>
+            <div class="add-to-cart">
+            <button id="checkout">Join</button>
+            </div>
+        </div>
+    `
+    });
+    booksContainer.innerHTML = html;
 }
---
+++ js/join-button.js
@@ -0,0 +1,29 @@
+const checkoutBtn = document.getElementById('checkout')
+const token = localStorage.getItem('token');
+checkoutBtn.addEventListener('click', function() {
+    fetch('https://simple-rest-api-production-6482.up.railway.app/enroll/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+    })
+    .then(response => {
+        if (response.status === 200) {
+            alert('Berhasil')
+        } else {
+            alert('Gagal')
+        }
+    })
+})
+
+const loginBtnContainer = document.querySelector('.loginBtnContainer')
+if (token) {
+    loginBtnContainer.innerHTML = '<button id="logout">Logout</button>'
+
+    document.getElementById("logout").addEventListener('click',
+    function() {
+        localStorage.removeItem('token')
+        window.location.href = './index.html'
+    })
+}
